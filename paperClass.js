3a99d7f40624b364d9ea7b5ea7412613d796bf5d
AniruddhNarayanan <72774377+AniruddhNarayanan@users.noreply.github.com>
2021-02-06 10:23:34 -0600

    Add files via upload

---
+++ paperClass.js
@@ -0,0 +1,24 @@
+class PaperClass{
+    constructor(x, y, width, height) {
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.body = Bodies.circle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+}
--- sketch.js
+++ sketch.js
@@ -1,13 +1,5 @@
-var paperSprite,paperBody,paperImage;
-var groundSprite,groundBody;
-var side1Sprite,side2Sprite,side3Sprite,dustbinImg;
-var side1Body,side2Body,side3Body;
-var options={
-	isStatic:false,
-	restitution:0.3,
-	friction:0.5,
-	density:1.2
-}
+
+
 //let c = color(255,204,0);
 //fill(c);
 
@@ -17,9 +9,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 function preload(){
-	paperImage=loadImage("circle.png");
-	dustbinImg=loadImage("dustbin.png");
-	
+
 }
 
 function setup() {
@@ -31,30 +21,7 @@ function setup() {
 	//making the sprites
     //creating the paper sprite
     //aperSprite=circle(110,185,30);
-	paperSprite=createSprite(110,185,10,10);
-	//adding the image to the sprite
-	paperSprite.addImage(paperImage);
-	// creating the ground
-	groundSprite=createSprite(400,195,800,10);
-	//adding color to the ground
-	groundSprite.shapeColor=color("yellow");
-	//creating the side1 sprite
-	side1Sprite=createSprite(700,185,100,10);
-	//adding color to the side 1
-	side1Sprite.addImage();
-	//creating the side2 sprite
-	side2Sprite=createSprite(750,155,10,70);
-	//adding color to the side 2
-	side2Sprite.shapeColor=color("pink");
-	//creating the side3 sprite
-	side3Sprite=createSprite(650,155,10,70);
-	//adding color to the side 3
-	side3Sprite.shapeColor=color("pink");
-	
-	//making the bodies for the sprites
-	//creating the paper body
-	paperBody=Bodies.circle(110,185,20,options);
-	World.add(world,paperBody);
+	paper = new PaperClass(100,100,20,20);
 	
 	groundBody=Bodies.rectangle(400,195,800,10,{isStatic:true});
 	World.add(world,groundBody);
@@ -68,29 +35,15 @@ function setup() {
 
 
 function draw() {
-  rectMode(CENTER);
-  background(255);
-  fill("red");
+ 
+  background(0);
   
-  groundSprite.x=groundBody.position.x;
-  groundSprite.y=groundBody.position.y;	
-
-  paper.x=paperBody.position.x
-  paper.y=paperBody.position.y;
-
+ paper.display();
   
-//	paper
-  drawSprites();
-
  
 }
 
 
 
-function keyPressed(){
-	if(keyCode===UP_ARROW){
-		paper.velocityX=6;
-		paper.velocityY=-4;
-	}
-}
+
 
